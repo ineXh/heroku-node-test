58455f9c53a5f064810927dac1442c21c48bd7c6
Anthony <anthonysiu@gmail.com>
2016-02-07 11:23:31 -0600

    443

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var port =  process.env.PORT || 80;
+var port =  process.env.PORT || 443;
 //app.set('port', (process.env.PORT || 5000));
 
 //app.use(express.static(__dirname + '/public'));
