00555f2737e62c2c85f8f2499e1f67456779c92e
Anthony <anthonysiu@gmail.com>
2016-02-07 11:49:07 -0600

    try

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm start
+web: node index_hero.js
---
+++ index_hero.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var port =  process.env.PORT || 80;
+//app.set('port', (process.env.PORT || 5000));
+
+//app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  //res.send('Hello World!');
+  res.sendfile(__dirname + '/public/index.html')
+});
+
+app.post('/', function (req, res) {
+  //res.send('Hello World!');
+  res.sendfile(__dirname + '/public/index.html')
+});
+
+
+/*var server = app.listen(port, function () {
+  console.log('Example app listening on port: ' + port);
+});*/
+var server = https.createServer(options, app).listen(port, function () {
+  console.log('Example app listening on port: ' + port);
+});
